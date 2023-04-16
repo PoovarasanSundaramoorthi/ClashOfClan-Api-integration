0e16419d1ab2c426bf535b80dc4dde518eb3caae
Poovarasan S <107316026+SundharaMoorthiPoov@users.noreply.github.com>
2023-04-16 09:33:25 -0700

    [COC] UI and api integration

---
+++ components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <>
+      <div className="hero-tamil">
+        <div className="bg-ground animate__animated animate__zoomInDown">
+          <h1 className="">Tamil4Evers</h1>
+        </div>
+      </div>
+
+      
+    </>
+  );
+}
